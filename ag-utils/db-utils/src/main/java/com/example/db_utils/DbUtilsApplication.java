c2f46d35787b2b62cbdece2f88a63c2353aec5ed
kyeongseo90 <rudtj9099@gmail.com>
2024-08-23 13:06:40 +0900

    feat: db-utils 모듈 추가

---
+++ ag-utils/db-utils/src/main/java/com/example/db_utils/DbUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.example.db_utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbUtilsApplication.class, args);
+	}
+
+}
