494af8a5cd4a46556849ca8f646446572c82bb9e
HyeonJu LEE <wkdal3512@dankook.ac.kr>
2024-08-23 14:11:40 +0900

    feat: api-gateway 모듈 추가

---
+++ ag-gateway/src/main/java/com/example/aggateway/AgGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.aggateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgGatewayApplication.class, args);
+	}
+
+}
