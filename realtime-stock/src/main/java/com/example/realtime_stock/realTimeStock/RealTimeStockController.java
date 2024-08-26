11a591ca771663b3be9fae0d40617997b31c169d
Elandland <bodan99@gmail.com>
2024-08-26 15:13:46 +0900

    Feat: 입력 받은 App Key와 App Secret Key의 갯수*40개 만큼 StockCode.txt에서 읽어 와서 실시간 데이터를 처리 하도록 구현

---
+++ realtime-stock/src/main/java/com/example/realtime_stock/realTimeStock/RealTimeStockController.java
@@ -0,0 +1,47 @@
+package com.example.realtime_stock.realTimeStock;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/stocks")
+public class RealTimeStockController {
+
+    private final RealTimeStockService webSocketClientService;
+
+    @Autowired
+    public RealTimeStockController(RealTimeStockService webSocketClientService) {
+        this.webSocketClientService = webSocketClientService;
+    }
+
+    @PostMapping("/start")
+    public String startWebSocket() throws IOException {
+        webSocketClientService.start();
+        return "웹 소켓 연결이 시작되었습니다.";
+    }
+
+
+    @PostMapping("/stop")
+    public String stopWebSocket(){
+        webSocketClientService.stop();
+        return "웹 소켓 연결 끝!";
+    }
+
+
+
+    @GetMapping("/{stockCode}")
+    public String getStockData(@PathVariable String stockCode) {
+        return webSocketClientService.getStockData(stockCode);
+    }
+
+    @GetMapping("/all")
+    public Map<String, String> getAllStockData() {
+        return webSocketClientService.getAllStockData();
+    }
+}
+
+
+
