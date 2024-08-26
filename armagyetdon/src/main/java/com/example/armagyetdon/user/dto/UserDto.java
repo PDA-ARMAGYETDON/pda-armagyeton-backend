90b0dc26bbc8253646831fb92676d2e064f7a9a3
HyeonJu LEE <wkdal3512@dankook.ac.kr>
2024-08-26 13:10:12 +0900

    fix: UserDto에 이메일 어노테이션 제거

--- armagyetdon/src/main/java/com/example/armagyetdon/user/dto/UserDto.java
+++ armagyetdon/src/main/java/com/example/armagyetdon/user/dto/UserDto.java
@@ -1,11 +1,8 @@
 package com.example.armagyetdon.user.dto;
 
-import jakarta.validation.constraints.Email;
-
 public class UserDto {
     private String loginId;
 
-    @Email
     private String email;
 
     private String name;
