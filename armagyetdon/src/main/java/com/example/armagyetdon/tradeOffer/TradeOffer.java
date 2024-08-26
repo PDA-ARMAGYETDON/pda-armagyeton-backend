f5c6057526389838964bac690604e3b2dc9cb0d3
kyeongseo90 <rudtj9099@gmail.com>
2024-08-26 20:11:20 +0900

    feat: 거래 제안/투표 entity 추가

---
+++ armagyetdon/src/main/java/com/example/armagyetdon/tradeOffer/TradeOffer.java
@@ -0,0 +1,37 @@
+package com.example.armagyetdon.tradeOffer;
+
+import com.example.armagyetdon.enums.OfferStatus;
+import com.example.armagyetdon.enums.TradeType;
+import com.example.armagyetdon.member.Member;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class TradeOffer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne(targetEntity = Member.class, fetch = FetchType.LAZY)
+    @JoinColumn(name="member_id")
+    private Member member;
+
+    @Enumerated(EnumType.STRING)
+    private TradeType tradeType;
+
+    private int recentPrice;
+    private int wantPrice;
+    private int quantity;
+    private LocalDateTime offerAt;
+
+    @Enumerated(EnumType.STRING)
+    private OfferStatus offerStatus;
+
+    private int upvotes;
+    private int downvotes;
+
+
+//    private int stockId;
+
+}
