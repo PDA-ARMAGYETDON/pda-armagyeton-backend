acdaa3d1e1a584a763d073f1211791da63892bee
HyeonJu LEE <wkdal3512@dankook.ac.kr>
2024-08-28 13:29:39 +0900

    rename: ag-gateway 패키지 이름 변경

--- ag-gateway/src/main/java/com/example/ag_gateway/AgGatewayApplication.java
+++ ag-gateway/src/main/java/com/example/ag_gateway/AgGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.example.aggateway;
+package com.example.ag_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- ag-gateway/src/test/java/com/example/ag_gateway/AgGatewayApplicationTests.java
+++ ag-gateway/src/test/java/com/example/ag_gateway/AgGatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.aggateway;
+package com.example.ag_gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
