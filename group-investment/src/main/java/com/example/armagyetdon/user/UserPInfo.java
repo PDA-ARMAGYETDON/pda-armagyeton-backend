06838e03c829ec790f186122720fedbf4264b174
Yoon <kohyc97@gmail.com>
2024-08-29 15:54:07 +0900

    feat: 비밀번호 Getter 추가

--- group-investment/src/main/java/com/example/armagyetdon/user/UserPInfo.java
+++ group-investment/src/main/java/com/example/armagyetdon/user/UserPInfo.java
@@ -1,15 +1,21 @@
 package com.example.armagyetdon.user;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
+@Getter
 @Entity
 public class UserPInfo {
+
     @Id
     @OneToOne
-    @JoinColumn(name="user_id")
+    @JoinColumn(name = "user_id")
     private User user;
 
     private String pInfo;
