ae49e7cc49120d02713468b6eacbc1638cacc969
HyeonJu LEE <wkdal3512@dankook.ac.kr>
2024-08-29 15:35:14 +0900

    feat: 매매 제안 dto 생성자 빌더 추가

--- group-investment/src/main/java/com/example/armagyetdon/tradeOffer/dto/TradeOfferDto.java
+++ group-investment/src/main/java/com/example/armagyetdon/tradeOffer/dto/TradeOfferDto.java
@@ -3,6 +3,7 @@ package com.example.armagyetdon.tradeOffer.dto;
 import com.example.armagyetdon.enums.TradeType;
 import com.example.armagyetdon.member.Member;
 import com.example.armagyetdon.tradeOffer.TradeOffer;
+import lombok.Builder;
 
 public class TradeOfferDto {
     private Member member;
@@ -11,5 +12,15 @@ public class TradeOfferDto {
     private int recentPrice;
     private int wantPrice;
     private int quantity;
+
+    @Builder
+    public TradeOfferDto(Member member, TradeType tradeType, String code, int recentPrice, int wantPrice, int quantity) {
+        this.member = member;
+        this.tradeType = tradeType;
+        this.code = code;
+        this.recentPrice = recentPrice;
+        this.wantPrice = wantPrice;
+        this.quantity = quantity;
+    }
 
 }
