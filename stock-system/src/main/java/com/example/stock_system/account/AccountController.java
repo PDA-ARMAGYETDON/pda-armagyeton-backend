cebcd28a1f5324b442cd07899959105bf2eb7007
Elandland <bodan99@gmail.com>
2024-08-29 10:46:41 +0900

    feat: Controller와 Service의 메소드명 일치시킴. 조회 메소드 추가

--- stock-system/src/main/java/com/example/stock_system/account/AccountController.java
+++ stock-system/src/main/java/com/example/stock_system/account/AccountController.java
@@ -13,21 +13,22 @@ public class AccountController {
     private final AccountService accountService;
 
     @GetMapping("/{id}")
-    public ApiResponse<AccountDto> getAccount(@PathVariable("id") int id) {
-        return new ApiResponse<>(200, true, "계좌를 조회했습니다.", null);
+    public ApiResponse<AccountDto> getAccount(@PathVariable("id") int id) throws Exception {
+        AccountDto accountDto = accountService.getAccount(id);
+        return new ApiResponse<>(200, true, "계좌를 조회했습니다.", accountDto);
     }
 
     @PostMapping("/personal")
     public ApiResponse<AccountDto> createPersonalAccount(@RequestBody CreateAccountRequest createAccountRequest){
-        Account savedAccount = accountService.savePersonalAccount();   //계정먼저 생성
-        accountService.saveAccountPInfo(savedAccount,createAccountRequest);  //비밀번호 연결해서 생성
+        Account savedAccount = accountService.createPersonalAccount();   //계정먼저 생성
+        accountService.createAccountPInfo(savedAccount,createAccountRequest);  //비밀번호 연결해서 생성
         return new ApiResponse<>(201, true, "계좌가 생성되었습니다.", null);
     }
 
     @PostMapping("/team")
     public ApiResponse<AccountDto> createTeamAccount(@RequestBody CreateAccountRequest createAccountRequest){
-        Account savedAccount = accountService.saveTeamAccount();   //계정먼저 생성
-        accountService.saveAccountPInfo(savedAccount,createAccountRequest);  //비밀번호 연결해서 생성
+        Account savedAccount = accountService.createTeamAccount();   //계정먼저 생성
+        accountService.createAccountPInfo(savedAccount,createAccountRequest);  //비밀번호 연결해서 생성
         return new ApiResponse<>(201, true, "모임 계좌가 생성되었습니다.", null);
     }
 
