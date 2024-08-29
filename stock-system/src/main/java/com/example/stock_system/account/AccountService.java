38ace8a1f0c3ae4a758e01c89162c83467e2ae65
Elandland <bodan99@gmail.com>
2024-08-29 10:46:35 +0900

    feat: Controller와 Service의 메소드명 일치시킴. 조회 메소드 추가

--- stock-system/src/main/java/com/example/stock_system/account/AccountService.java
+++ stock-system/src/main/java/com/example/stock_system/account/AccountService.java
@@ -1,8 +1,14 @@
 package com.example.stock_system.account;
 
+import com.example.stock_system.account.dto.AccountDto;
+import com.example.stock_system.account.dto.AccountDtoConverter;
 import com.example.stock_system.account.dto.CreateAccountRequest;
+import com.example.stock_system.account.exception.AccountErrorCode;
+import com.example.stock_system.account.exception.AccountException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 import java.util.Random;
 
 @Service
@@ -11,8 +17,14 @@ public class AccountService {
 
     private final AccountRepository accountRepository;
     private final AccountPInfoRepository accountPInfoRepository;
+    private final AccountDtoConverter accountDtoConverter;
+    public AccountDto getAccount(int id) throws Exception {
+        Account account = accountRepository.findById(id).orElseThrow(
+                ()->new AccountException(AccountErrorCode.ACCOUNT_NOT_FOUND));
+        return accountDtoConverter.fromEntity(account);
+    }
 
-    public Account saveTeamAccount(){
+    public Account createTeamAccount(){
 
         //랜덤으로 11자 계좌 만들어주기
         //팀 계좌 81902 뒤에 6자 난수
@@ -22,7 +34,7 @@ public class AccountService {
         return accountRepository.save(account);
     }
 
-    public Account savePersonalAccount(){
+    public Account createPersonalAccount(){
 
         //개인 계좌는 81901 뒤에 6자 난수로 뽑기
         String accountNumber = "81901"+ generateRandomNumber();
@@ -31,15 +43,15 @@ public class AccountService {
         return accountRepository.save(account);
     }
 
-    public void saveAccountPInfo(Account account, CreateAccountRequest accountPInfo){
+    public void createAccountPInfo(Account account, CreateAccountRequest accountPInfo){
         AccountPInfo createdAccountPInfo = new AccountPInfo(account,accountPInfo.getAccountPInfo());
         accountPInfoRepository.save(createdAccountPInfo);
     }
 
     private static String generateRandomNumber() {
         Random random = new Random();
-        int randomNumber = random.nextInt(1000000); // 0부터 999999까지의 난수 생성
-        return String.format("%06d", randomNumber); // 6자리로 포맷팅
+        int randomNumber = random.nextInt(1000000);
+        return String.format("%06d", randomNumber);
     }
 
 }
