31a053a316dba005ac8de5d93ed23d6925998901
HyeonJu LEE <wkdal3512@dankook.ac.kr>
2024-08-29 15:23:05 +0900

    feat: 매매 제안 controller 생성

---
+++ group-investment/src/main/java/com/example/armagyetdon/tradeOffer/TradeOfferController.java
@@ -0,0 +1,13 @@
+package com.example.armagyetdon.tradeOffer;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/trade-offer")
+public class TradeOfferController {
+}
