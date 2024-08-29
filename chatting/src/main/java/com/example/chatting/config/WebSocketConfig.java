eccc0efb048c16ffb3d9c8aa2cc6a52cb5359da4
ymkdev <eveminful@gmail.com>
2024-08-30 02:50:20 +0900

    fix: RabbitMQ config 및 관련 코드 삭제
    #21

--- chatting/src/main/java/com/example/chatting/config/WebSocketConfig.java
+++ chatting/src/main/java/com/example/chatting/config/WebSocketConfig.java
@@ -1,9 +1,7 @@
 package com.example.chatting.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.util.AntPathMatcher;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -11,43 +9,6 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-//    @Value("${chat.path}")
-//    private String path;
-//
-//    @Value("localhost")
-//    private String rabbitmqHost;
-//
-//    @Value("5672")
-//    private int rabbitmqPort;
-//
-//    @Value("guest")
-//    private String rabbitmqUsername;
-//
-//    @Value("guest")
-//    private String rabbitmqPassword;
-//
-//    @Override
-//    public void registerStompEndpoints(StompEndpointRegistry registry) {
-//        registry.addEndpoint("/stomp/chat").setAllowedOrigins("*");
-//        //registry.addEndpoint("/ws").setAllowedOrigins(path).withSockJS();
-//    }
-//
-//    public void configureMessageBroker(MessageBrokerRegistry registry) {
-//        registry.setPathMatcher(new AntPathMatcher(".")).setApplicationDestinationPrefixes("/pub");
-//
-//        registry.enableStompBrokerRelay("/queue","/topic","/exchange", "/amq/queue")
-//                .setClientLogin(rabbitmqUsername)
-//                .setClientPasscode(rabbitmqPassword)
-//                .setVirtualHost("/");
-////                .setSystemLogin(rabbitmqUsername)
-////                .setSystemPasscode(rabbitmqPassword)
-////                .setRelayHost(rabbitmqHost)
-////                .setRelayPort(rabbitmqPort);
-////                .setVirtualHost(rabbitmqHost)
-////                .setClientLogin(rabbitmqUsername)
-////                .setSystemPasscode(rabbitmqPassword);
-//    }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
