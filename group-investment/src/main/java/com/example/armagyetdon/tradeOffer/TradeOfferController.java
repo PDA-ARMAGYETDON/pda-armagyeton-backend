87b3ed957e77cc93deea34663161487363b3fb61
HyeonJu LEE <wkdal3512@dankook.ac.kr>
2024-08-29 15:23:28 +0900

    feat: 매매 제안 생성 api 생성

--- group-investment/src/main/java/com/example/armagyetdon/tradeOffer/TradeOfferController.java
+++ group-investment/src/main/java/com/example/armagyetdon/tradeOffer/TradeOfferController.java
@@ -1,5 +1,7 @@
 package com.example.armagyetdon.tradeOffer;
 
+import com.example.armagyetdon.tradeOffer.dto.CreateTradeOfferRequest;
+import com.example.common.dto.ApiResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,4 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 @RequestMapping("/api/trade-offer")
 public class TradeOfferController {
+    private final TradeOfferService tradeOfferService;
+
+    @PostMapping()
+    public ApiResponse createTradeOffer(@RequestBody CreateTradeOfferRequest createTradeOfferRequest) {
+        tradeOfferService.createTradeOffer(createTradeOfferRequest);
+        return new ApiResponse(201, true, "매매제안을 생성했습니다.", null);
+    }
 }
