1c1ee790ee534b5d41026a37b46b32408ec4f69c
Elandland <bodan99@gmail.com>
2024-08-29 09:59:29 +0900

    feat: JPA사용을 위해 pk를 위한 id 추가 후 accountId와 @MapsId를 통해 매핑

--- stock-system/src/main/java/com/example/stock_system/account/AccountPInfo.java
+++ stock-system/src/main/java/com/example/stock_system/account/AccountPInfo.java
@@ -1,6 +1,7 @@
 package com.example.stock_system.account;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
@@ -8,7 +9,10 @@ import java.time.LocalDateTime;
 @Entity
 public class AccountPInfo {
     @Id
+    private int id;
+
     @OneToOne
+    @MapsId
     @JoinColumn(name = "account_id")
     private Account account;
 
@@ -16,4 +20,14 @@ public class AccountPInfo {
 
     @UpdateTimestamp
     private LocalDateTime updatedAt;
+
+    @Builder
+    public AccountPInfo(Account acccount, String accountPInfo){
+        this.account = acccount;
+        this.pInfo = accountPInfo;
+    }
+
+    public AccountPInfo() {
+
+    }
 }
