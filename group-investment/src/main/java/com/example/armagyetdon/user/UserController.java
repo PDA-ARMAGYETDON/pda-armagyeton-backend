892d95894206a10015f60035ecfd91c0074cc510
Yoon <kohyc97@gmail.com>
2024-08-29 15:51:47 +0900

    feat: 로그인 controller 생성

--- group-investment/src/main/java/com/example/armagyetdon/user/UserController.java
+++ group-investment/src/main/java/com/example/armagyetdon/user/UserController.java
@@ -1,12 +1,10 @@
 package com.example.armagyetdon.user;
 
+import com.example.armagyetdon.user.dto.LoginRequestDto;
 import com.example.armagyetdon.user.dto.UserDto;
 import com.example.common.dto.ApiResponse;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
@@ -18,6 +16,14 @@ public class UserController {
     // ApiResponse를 사용해서 response 형식을 통일해주세요.
     @GetMapping("/{id}")
     public ApiResponse<UserDto> getUser(@PathVariable String id) {
+
         return new ApiResponse<>(200, true, "사용자를 조회했습니다.", null);
+    }
+
+    @PostMapping("/login")
+    public ApiResponse<LoginRequestDto> login(@RequestBody LoginRequestDto loginRequest) {
+
+        userService.login(loginRequest);
+        return new ApiResponse<>(200, true, "로그인에 성공하였습니다.", null);
     }
 }
