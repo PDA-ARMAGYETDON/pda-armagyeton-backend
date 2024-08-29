2494d6fffaba1cf18d32899d6ab51f69b3741ced
Yoon <kohyc97@gmail.com>
2024-08-29 15:52:25 +0900

    feat: 유저 로그인 서비스 생성(보안 X)

--- group-investment/src/main/java/com/example/armagyetdon/user/UserService.java
+++ group-investment/src/main/java/com/example/armagyetdon/user/UserService.java
@@ -1,5 +1,8 @@
 package com.example.armagyetdon.user;
 
+import com.example.armagyetdon.user.dto.LoginRequestDto;
+import com.example.armagyetdon.user.exception.UserErrorCode;
+import com.example.armagyetdon.user.exception.UserException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -7,4 +10,18 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final UserPInfoRepository userPInfoRepository;
+
+    public void login(LoginRequestDto loginRequest) {
+
+        User foundedUser = userRepository.findByLoginId(loginRequest.getUserId())
+                .orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_FOUND));
+
+        UserPInfo foundedUserPInfo = userPInfoRepository.findById(foundedUser.getId())
+                .orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_FOUND));
+
+        if (!loginRequest.getPInfo().equals(foundedUserPInfo.getPInfo())) {
+            throw new UserException(UserErrorCode.USER_NOT_FOUND);
+        }
+    }
 }
