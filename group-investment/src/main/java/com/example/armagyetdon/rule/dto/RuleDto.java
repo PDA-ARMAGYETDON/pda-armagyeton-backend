97cc2972a20b00584c815996ed58250780fbd60f
ymkdev <eveminful@gmail.com>
2024-08-30 11:10:07 +0900

    feat: 규칙 dto 생성자 빌더 추가
    #33

--- group-investment/src/main/java/com/example/armagyetdon/rule/dto/RuleDto.java
+++ group-investment/src/main/java/com/example/armagyetdon/rule/dto/RuleDto.java
@@ -1,11 +1,15 @@
 package com.example.armagyetdon.rule.dto;
 
+import com.example.armagyetdon.rule.Rule;
+import com.example.armagyetdon.team.Team;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.util.Date;
 
 @Getter
 public class RuleDto {
+    private Team team;
     private float prdyVrssRt;
     private int urgentTradeUpvotes;
     private int tradeUpvotes;
@@ -14,4 +18,18 @@ public class RuleDto {
     private Date payDate;
     private float maxLossRt;
     private float maxProfitRt;
+
+    @Builder
+    public RuleDto(Team team, float prdyVrssRt, int urgentTradeUpvotes, int tradeUpvotes, int depositAmt, Date period, Date payDate, float maxLossRt, float maxProfitRt) {
+        this.team = team;
+        this.prdyVrssRt = prdyVrssRt;
+        this.urgentTradeUpvotes = urgentTradeUpvotes;
+        this.tradeUpvotes = tradeUpvotes;
+        this.depositAmt = depositAmt;
+        this.period = period;
+        this.payDate = payDate;
+        this.maxLossRt = maxLossRt;
+        this.maxProfitRt = maxProfitRt;
+    }
+
 }
