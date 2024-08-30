34a253d677f1332d65731ccf9c7b5a80d9fa1aaa
ymkdev <eveminful@gmail.com>
2024-08-30 10:39:16 +0900

    feat: 초대 dto 생성자 빌더 추가
    #33

--- group-investment/src/main/java/com/example/armagyetdon/team/dto/InvitationDto.java
+++ group-investment/src/main/java/com/example/armagyetdon/team/dto/InvitationDto.java
@@ -2,9 +2,17 @@ package com.example.armagyetdon.team.dto;
 
 import com.example.armagyetdon.team.Invitation;
 import com.example.armagyetdon.team.Team;
+import lombok.Builder;
 
 public class InvitationDto {
     private Team team;
     private String inviteCode;
     private String inviteUrl;
 
+    @Builder
+    public InvitationDto(Team team, String inviteCode, String inviteUrl) {
+        this.team = team;
+        this.inviteCode = inviteCode;
+        this.inviteUrl = inviteUrl;
+    }
+}
