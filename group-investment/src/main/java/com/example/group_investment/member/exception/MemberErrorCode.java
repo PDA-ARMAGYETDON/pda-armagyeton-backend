15a2e11a23f99681b832e345b1254225ff88b9ad
kyeongseo90 <rudtj9099@gmail.com>
2024-08-30 17:31:06 +0900

    feat: member에 exception 추가

---
+++ group-investment/src/main/java/com/example/group_investment/member/exception/MemberErrorCode.java
@@ -0,0 +1,19 @@
+package com.example.group_investment.member.exception;
+
+import lombok.Getter;
+
+@Getter
+public enum MemberErrorCode {
+    MEMBER_NOT_FOUND(404, "AG401", "멤버가 존재하지 않습니다."),
+    ;
+
+    private final int status;
+    private final String divisionCode;
+    private final String message;
+
+    MemberErrorCode(int status, String divisionCode, String message) {
+        this.status = status;
+        this.divisionCode = divisionCode;
+        this.message = message;
+    }
+}
