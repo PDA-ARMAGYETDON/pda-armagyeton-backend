6e95c3036bfe061f7e6ce5a7da1adfffea047df0
ymkdev <eveminful@gmail.com>
2024-08-30 10:51:58 +0900

    feat: 팀 엔티티 생성자 빌더 추가
    #33

--- group-investment/src/main/java/com/example/armagyetdon/team/Team.java
+++ group-investment/src/main/java/com/example/armagyetdon/team/Team.java
@@ -3,7 +3,9 @@ package com.example.armagyetdon.team;
 import com.example.armagyetdon.enums.Category;
 import com.example.armagyetdon.enums.TeamStatus;
 import com.example.armagyetdon.member.Member;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
 import jakarta.persistence.*;
+import lombok.Builder;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
@@ -31,4 +33,18 @@ public class Team {
 
     @OneToMany(mappedBy = "team")
     private List<Member> members = new ArrayList<>();
+
+    @Builder
+    public Team(String name, Category category, TeamStatus status, LocalDateTime startAt, LocalDateTime endAt, LocalDateTime createdAt) {
+        this.name = name;
+        this.category = category;
+        this.status = status;
+        this.startAt = startAt;
+        this.endAt = endAt;
+        this.createdAt = createdAt;
+    }
+
+    public Team() {
+
+    }
 }
