0844a51c6d9345321f5ca46ec73a7d4f5fb73fb3
ymkdev <eveminful@gmail.com>
2024-08-30 10:47:25 +0900

    feat: 팀 dto toEntity() 메소드 생성
    #33

--- group-investment/src/main/java/com/example/armagyetdon/team/dto/TeamDto.java
+++ group-investment/src/main/java/com/example/armagyetdon/team/dto/TeamDto.java
@@ -2,6 +2,7 @@ package com.example.armagyetdon.team.dto;
 
 import com.example.armagyetdon.enums.Category;
 import com.example.armagyetdon.enums.TeamStatus;
+import com.example.armagyetdon.team.Team;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -14,6 +15,8 @@ public class TeamDto {
     private TeamStatus status;
     private LocalDateTime startAt;
     private LocalDateTime endAt;
+    private LocalDateTime createdAt;
+
     @Builder
     public TeamDto(String name, Category category, TeamStatus status, LocalDateTime startAt, LocalDateTime endAt, LocalDateTime createdAt) {
         this.name = name;
@@ -22,5 +25,16 @@ public class TeamDto {
         this.startAt = startAt;
         this.endAt = endAt;
         this.createdAt = LocalDateTime.now();
+    }
+
+    public Team toEntity() {
+        return Team.builder()
+                .name(this.name)
+                .category(this.category)
+                .status(this.status)
+                .startAt(this.startAt)
+                .endAt(this.endAt)
+                .createdAt(this.createdAt)
+                .build();
     }
 }
