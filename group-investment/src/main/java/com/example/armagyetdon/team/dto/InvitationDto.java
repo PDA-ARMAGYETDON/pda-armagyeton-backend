f229279ad4a8b33ecdc95a6ec2954f93d8987d5f
ymkdev <eveminful@gmail.com>
2024-08-30 10:38:44 +0900

    feat: 초대 dto 생성
    #33

---
+++ group-investment/src/main/java/com/example/armagyetdon/team/dto/InvitationDto.java
@@ -0,0 +1,10 @@
+package com.example.armagyetdon.team.dto;
+
+import com.example.armagyetdon.team.Invitation;
+import com.example.armagyetdon.team.Team;
+
+public class InvitationDto {
+    private Team team;
+    private String inviteCode;
+    private String inviteUrl;
+
