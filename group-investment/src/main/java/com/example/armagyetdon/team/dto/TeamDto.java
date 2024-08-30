30e71537f56e67d00ec659531c76e0af317f400d
ymkdev <eveminful@gmail.com>
2024-08-30 10:46:18 +0900

    feat: 팀 dto 생성자 빌더 추가
    #33

--- group-investment/src/main/java/com/example/armagyetdon/team/dto/TeamDto.java
+++ group-investment/src/main/java/com/example/armagyetdon/team/dto/TeamDto.java
@@ -2,14 +2,25 @@ package com.example.armagyetdon.team.dto;
 
 import com.example.armagyetdon.enums.Category;
 import com.example.armagyetdon.enums.TeamStatus;
+import lombok.Builder;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
+@Getter
 public class TeamDto {
     private String name;
     private Category category;
     private TeamStatus status;
     private LocalDateTime startAt;
     private LocalDateTime endAt;
-    private LocalDateTime createdAt = LocalDateTime.now();
+    @Builder
+    public TeamDto(String name, Category category, TeamStatus status, LocalDateTime startAt, LocalDateTime endAt, LocalDateTime createdAt) {
+        this.name = name;
+        this.category = category;
+        this.status = status;
+        this.startAt = startAt;
+        this.endAt = endAt;
+        this.createdAt = LocalDateTime.now();
+    }
 }
