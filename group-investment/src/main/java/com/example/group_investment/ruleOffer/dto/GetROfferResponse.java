1a149b8da7fa1013601f40aff21df6ccb17f8e63
kyeongseo90 <rudtj9099@gmail.com>
2024-08-30 18:14:44 +0900

    feat: rule get offer 디시리얼라이즈를 위한 getter 추가

--- group-investment/src/main/java/com/example/group_investment/ruleOffer/dto/GetROfferResponse.java
+++ group-investment/src/main/java/com/example/group_investment/ruleOffer/dto/GetROfferResponse.java
@@ -1,10 +1,12 @@
 package com.example.group_investment.ruleOffer.dto;
 
 import lombok.Builder;
+import lombok.Getter;
 
 import java.util.List;
 
 @Builder
+@Getter
 public class GetROfferResponse {
     List<GetROfferResponsePayFee> payFeeOffers;
     List<GetROfferResponseUrgentSale> urgentSaleOffers;
--- group-investment/src/main/java/com/example/group_investment/ruleOffer/dto/GetROfferResponseType.java
+++ group-investment/src/main/java/com/example/group_investment/ruleOffer/dto/GetROfferResponseType.java
@@ -1,4 +1,5 @@
 package com.example.group_investment.ruleOffer.dto;
 
+
 public interface GetROfferResponseType {
 }
