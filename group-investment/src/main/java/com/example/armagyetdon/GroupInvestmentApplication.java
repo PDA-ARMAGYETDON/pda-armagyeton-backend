96c529c5d102796aab9eab2a2c3983eedcd03e97
HyeonJu LEE <wkdal3512@dankook.ac.kr>
2024-08-30 14:50:14 +0900

    fix: 각 모듈의 Application 클래스에 공통 모듈을 스캔할 수 있도록 @ComponentScan 어노테이션 설정

--- group-investment/src/main/java/com/example/armagyetdon/GroupInvestmentApplication.java
+++ group-investment/src/main/java/com/example/armagyetdon/GroupInvestmentApplication.java
@@ -2,8 +2,10 @@ package com.example.armagyetdon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.common", "com.example.armagyetdon"})
 public class GroupInvestmentApplication {
 
     public static void main(String[] args) {
