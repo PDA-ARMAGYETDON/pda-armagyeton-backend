facc1f539c5ddfd0ea611f19dae45d25b20d38b9
ymkdev <eveminful@gmail.com>
2024-08-30 10:37:24 +0900

    feat: 초대 Repository 생성
    #33

---
+++ group-investment/src/main/java/com/example/armagyetdon/team/InvitationRepository.java
@@ -0,0 +1,6 @@
+package com.example.armagyetdon.team;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InvitationRepository extends JpaRepository<Invitation, Integer> {
+}
