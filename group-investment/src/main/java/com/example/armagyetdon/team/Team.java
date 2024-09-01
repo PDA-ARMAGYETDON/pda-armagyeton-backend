d95766a13f3b5b5bc9491c18f7a7909513035093
ymkdev <eveminful@gmail.com>
2024-09-01 12:45:35 +0900

    feat: Team 엔티티에 user 추가
    #36

--- group-investment/src/main/java/com/example/armagyetdon/team/Team.java
+++ group-investment/src/main/java/com/example/armagyetdon/team/Team.java
@@ -3,15 +3,18 @@ package com.example.armagyetdon.team;
 import com.example.armagyetdon.enums.Category;
 import com.example.armagyetdon.enums.TeamStatus;
 import com.example.armagyetdon.member.Member;
+import com.example.armagyetdon.user.User;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import jakarta.persistence.*;
 import lombok.Builder;
+import lombok.Getter;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
 @Entity
 public class Team {
     @Id
@@ -34,14 +37,20 @@ public class Team {
     @OneToMany(mappedBy = "team")
     private List<Member> members = new ArrayList<>();
 
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
     @Builder
-    public Team(String name, Category category, TeamStatus status, LocalDateTime startAt, LocalDateTime endAt, LocalDateTime createdAt) {
+    public Team(String name, Category category, TeamStatus status, LocalDateTime startAt, LocalDateTime endAt, LocalDateTime createdAt, User user) {
         this.name = name;
         this.category = category;
         this.status = status;
         this.startAt = startAt;
         this.endAt = endAt;
         this.createdAt = createdAt;
+        this.user = user;
     }
 
     public Team() {
