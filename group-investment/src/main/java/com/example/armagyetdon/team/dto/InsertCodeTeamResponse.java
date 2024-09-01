c7b26c3ae36f6f5d6c0bb71ca6c2dccfd707fb95
ymkdev <eveminful@gmail.com>
2024-09-01 20:50:17 +0900

    feat: 초대 코드 기입 시 응답 dto 추가
    #39

---
+++ group-investment/src/main/java/com/example/armagyetdon/team/dto/InsertCodeTeamResponse.java
@@ -0,0 +1,10 @@
+package com.example.armagyetdon.team.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class InsertCodeTeamResponse {
+    private int groupId;
+}
