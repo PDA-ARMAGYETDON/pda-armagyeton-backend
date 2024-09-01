ac75069eb6cd884b8d0e6ca20cf2722f81663467
kyeongseo <rudtj9099@gmail.com>
2024-09-02 01:20:21 +0900

    feat: 회원가입 로직 작성

--- group-investment/src/main/java/com/example/armagyetdon/user/UserRepository.java
+++ group-investment/src/main/java/com/example/armagyetdon/user/UserRepository.java
@@ -3,6 +3,10 @@ package com.example.armagyetdon.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Boolean existsByLoginId(String loginId);
+    Optional<User> findByLoginId(String loginId);
 }
