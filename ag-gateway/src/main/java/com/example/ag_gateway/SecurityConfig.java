558743d1cefed572246d1b15d1e2d0163f111ed7
kyeongseo <rudtj9099@gmail.com>
2024-09-02 01:16:23 +0900

    feat: gateway security config

--- ag-gateway/src/main/java/com/example/ag_gateway/SecurityConfig.java
+++ ag-gateway/src/main/java/com/example/ag_gateway/SecurityConfig.java
@@ -3,30 +3,36 @@ package com.example.ag_gateway;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebFluxSecurity
+//@EnableWebFluxSecurity
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests((requests) ->
-                        requests
+                .csrf(AbstractHttpConfigurer::disable);
+
+        http
+                .formLogin(AbstractHttpConfigurer::disable);
+
+        http
+                .httpBasic(AbstractHttpConfigurer::disable);
+
+
+
+        http
+                .authorizeHttpRequests((requests) -> requests
                                 .requestMatchers("/signin", "/signup").permitAll()
                                 .requestMatchers("/").hasRole("USER")
                                 .anyRequest().authenticated())
-                .formLogin((formLogin) ->
-                        formLogin
-                                .loginPage("/signin")
-                                .usernameParameter("username")
-                                .passwordParameter("password")
-                                .defaultSuccessUrl("/", true))
                 .logout((logoutConfig)->
                         logoutConfig
                                 .logoutSuccessUrl("/"))
@@ -36,6 +42,10 @@ public class SecurityConfig {
 //                                .accessDeniedPage(accessDeniedHandler))
 //                .userDetailsService(myUserDetailsService());
         ;
+
+        http
+                .sessionManagement((session)->session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
