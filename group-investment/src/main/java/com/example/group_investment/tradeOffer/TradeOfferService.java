1456155305d1c00c90d8adfc0da392ef510f18de
HyeonJu LEE <wkdal3512@dankook.ac.kr>
2024-09-02 13:03:33 +0900

    rename: 패키지 이름 변경 반영(armagyetdon -> group_investment)

--- group-investment/src/main/java/com/example/group_investment/tradeOffer/TradeOfferService.java
+++ group-investment/src/main/java/com/example/group_investment/tradeOffer/TradeOfferService.java
@@ -1,19 +1,31 @@
-package com.example.armagyetdon.tradeOffer;
+package com.example.group_investment.tradeOffer;
 
-import com.example.armagyetdon.member.Member;
-import com.example.armagyetdon.member.MemberRepository;
-import com.example.armagyetdon.member.exception.MemberErrorCode;
-import com.example.armagyetdon.member.exception.MemberException;
-import com.example.armagyetdon.tradeOffer.dto.CreateTradeOfferRequest;
-import com.example.armagyetdon.tradeOffer.dto.TradeOfferDto;
+import com.example.group_investment.enums.TradeType;
+import com.example.group_investment.member.Member;
+import com.example.group_investment.member.MemberRepository;
+import com.example.group_investment.member.exception.MemberErrorCode;
+import com.example.group_investment.member.exception.MemberException;
+import com.example.group_investment.team.Team;
+import com.example.group_investment.team.TeamRepository;
+import com.example.group_investment.team.exception.TeamErrorCode;
+import com.example.group_investment.team.exception.TeamException;
+import com.example.group_investment.tradeOffer.dto.*;
+import com.example.group_investment.tradeOffer.exception.TradeOfferErrorCode;
+import com.example.group_investment.tradeOffer.exception.TradeOfferException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class TradeOfferService {
     private final TradeOfferRepository tradeOfferRepository;
     private final MemberRepository memberRepository;
+    private final WebClient.Builder webClientBuilder;
+    private final TeamRepository teamRepository;
 
     public void createTradeOffer(CreateTradeOfferRequest createTradeOfferRequest) {
         // FIXME: 토큰으로 사용자 아이디와 모임 아이디 가져와야함
@@ -23,19 +35,23 @@ public class TradeOfferService {
         Member member = memberRepository.findByUserIdAndTeamId(userId, teamId).orElseThrow(
                 () -> new MemberException(MemberErrorCode.MEMBER_NOT_FOUND));
 
+        Team team = teamRepository.findById(teamId).orElseThrow(
+                () -> new TeamException(TeamErrorCode.TEAM_NOT_FOUND));
+
         TradeOfferDto tradeOfferDto = TradeOfferDto.builder()
                 .member(member)
+                .team(team)
                 .tradeType(createTradeOfferRequest.getTradeType())
                 .recentPrice(createTradeOfferRequest.getRecentPrice())
                 .wantPrice(createTradeOfferRequest.getWantPrice())
                 .quantity(createTradeOfferRequest.getQuantity())
                 .code(createTradeOfferRequest.getCode())
                 .build();
-
         try {
             tradeOfferRepository.save(tradeOfferDto.toEntity());
         } catch (Exception e) {
             throw new MemberException(MemberErrorCode.TRADE_OFFER_SAVE_FAILED);
         }
     }
+
 }
