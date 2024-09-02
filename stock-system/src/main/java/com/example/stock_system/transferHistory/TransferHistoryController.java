5139bceae5ee2469a4a0a308a34875b020b8562d
Yoon <kohyc97@gmail.com>
2024-09-02 09:54:27 +0900

    feat: 내 계좌 이체내역 Controller 구현

--- stock-system/src/main/java/com/example/stock_system/transferHistory/TransferHistoryController.java
+++ stock-system/src/main/java/com/example/stock_system/transferHistory/TransferHistoryController.java
@@ -1,10 +1,20 @@
 package com.example.stock_system.transferHistory;
 
+import com.example.stock_system.transferHistory.dto.AccountTransferDetailDto;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
 public class TransferHistoryController {
     private final TransferHistoryService transferHistoryService;
+
+    @PostMapping("accounts/{id}/transactions")
+    public AccountTransferDetailDto getAccountTransferDetail(@PathVariable int userId) {
+
+        return transferHistoryService.getAccountTransferDetail(userId);
+
+    }
 }
