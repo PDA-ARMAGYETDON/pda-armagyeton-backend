a689e384960a6902fa0b097e717c8fee508ef6fd
ymkdev <eveminful@gmail.com>
2024-09-02 15:17:11 +0900

    fix: 필요 없는 코드 삭제
    #42

--- group-investment/src/main/java/com/example/group_investment/team/TeamController.java
+++ group-investment/src/main/java/com/example/group_investment/team/TeamController.java
@@ -1,13 +1,11 @@
 package com.example.group_investment.team;
 
 import com.example.group_investment.team.dto.*;
-import com.example.group_investment.team.dto.TeamDto;
 import com.example.group_investment.team.dto.CreateTeamRequest;
 import com.example.group_investment.team.dto.CreateTeamResponse;
 import com.example.common.dto.ApiResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
@@ -21,19 +19,13 @@ public class TeamController {
 
     // TeamResponseDto를 만들어서 response해주세요.
     // ApiResponse를 사용해서 response 형식을 통일해주세요.
-    @GetMapping("/{id}")
-    public ApiResponse<TeamDto> getTeam(@PathVariable int id) {
-        return new ApiResponse<>(200, true, "팀 정보를 조회했습니다.", new TeamDto());
-    }
-
-
 
     @PostMapping("")
     public ApiResponse<CreateTeamResponse> createTeam(@RequestBody CreateTeamRequest createTeamRequest) {
         CreateTeamResponse createTeamResponse = teamService.createTeam(createTeamRequest);
         return new ApiResponse<>(201, true, "팀을 생성했습니다.", createTeamResponse);
     }
-    
+
     @GetMapping()
     public ApiResponse<InsertCodeTeamResponse> insertCode(@RequestParam(value = "inviteCode", required = true) String inviteCode) {
         InsertCodeTeamResponse insertCodeTeamResponse = teamService.insertCode(inviteCode);
