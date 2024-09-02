9f2b3d24f694b6fae089141a8ba53dac49be60bc
Yoon <kohyc97@gmail.com>
2024-09-02 09:53:17 +0900

    feat: 이체내역 Service 구현

--- stock-system/src/main/java/com/example/stock_system/transferHistory/TransferHistoryService.java
+++ stock-system/src/main/java/com/example/stock_system/transferHistory/TransferHistoryService.java
@@ -1,10 +1,22 @@
 package com.example.stock_system.transferHistory;
 
+import com.example.stock_system.transferHistory.dto.AccountTransferDetailDto;
+import com.example.stock_system.transferHistory.exception.TransferHistoryErrorCode;
+import com.example.stock_system.transferHistory.exception.TransferHistoryException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class TransferHistoryService {
+
     private final TransferHistoryRepository transferHistoryRepository;
+
+    public AccountTransferDetailDto getAccountTransferDetail(int userId) {
+        return transferHistoryRepository
+                .findByAccountId(userId).orElseThrow(
+                        () -> new TransferHistoryException(TransferHistoryErrorCode.TRANSFER_HISTORY_NOT_FOUND)
+                );
+    }
+
 }
