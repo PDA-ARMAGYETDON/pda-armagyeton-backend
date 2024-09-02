43ccfe14009910a337e0beda5e40a7e6fbb47bf3
Yoon <kohyc97@gmail.com>
2024-09-02 17:59:18 +0900

    feat: 거래내역 조회 로직 추가(성능 개선 필요)

--- stock-system/src/main/java/com/example/stock_system/transferHistory/TransferHistoryService.java
+++ stock-system/src/main/java/com/example/stock_system/transferHistory/TransferHistoryService.java
@@ -1,22 +1,46 @@
 package com.example.stock_system.transferHistory;
 
+import com.example.stock_system.account.Account;
+import com.example.stock_system.account.AccountRepository;
+import com.example.stock_system.account.TeamAccountRepository;
+import com.example.stock_system.account.exception.AccountErrorCode;
+import com.example.stock_system.account.exception.AccountException;
 import com.example.stock_system.transferHistory.dto.AccountTransferDetailDto;
 import com.example.stock_system.transferHistory.exception.TransferHistoryErrorCode;
 import com.example.stock_system.transferHistory.exception.TransferHistoryException;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class TransferHistoryService {
 
+    private static final Logger log = LoggerFactory.getLogger(TransferHistoryService.class);
     private final TransferHistoryRepository transferHistoryRepository;
+    private final AccountRepository accountRepository;
+    private final TeamAccountRepository teamAccountRepository;
 
-    public AccountTransferDetailDto getAccountTransferDetail(int userId) {
-        return transferHistoryRepository
-                .findByAccountId(userId).orElseThrow(
-                        () -> new TransferHistoryException(TransferHistoryErrorCode.TRANSFER_HISTORY_NOT_FOUND)
-                );
+    @Transactional
+    public List<AccountTransferDetailDto> getAccountTransferDetail(int userId) {
+        List<AccountTransferDetailDto> foundedTransferDetailList = null;
+
+        List<Account> foundedAccount = accountRepository.findByUserId(userId)
+                .orElseThrow(() -> new AccountException(AccountErrorCode.USER_ACCOUNT_NOT_FOUND));
+
+        //TODO: 성능 개선 필요
+        for (Account account : foundedAccount) {
+            if (!teamAccountRepository.existsByAccount(account)) {
+                foundedTransferDetailList =
+                        transferHistoryRepository.findByAccountId(account.getId())
+                                .orElseThrow(() -> new TransferHistoryException(TransferHistoryErrorCode.TRANSFER_HISTORY_NOT_FOUND));
+            }
+        }
+        return foundedTransferDetailList;
     }
 
 }
