2315098f8f33ad4f9e8f30cd1c4ab0809a7f2a08
ymkdev <eveminful@gmail.com>
2024-09-02 15:15:57 +0900

    fix: 팀 엔티티 구조 변경으로 코드 수정
    #42

--- group-investment/src/main/java/com/example/group_investment/team/TeamService.java
+++ group-investment/src/main/java/com/example/group_investment/team/TeamService.java
@@ -25,7 +25,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.security.SecureRandom;
 import java.util.List;
 
-
 @Service
 @AllArgsConstructor
 public class TeamService {
@@ -34,7 +33,7 @@ public class TeamService {
     private final RuleRepository ruleRepository;
 
     private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    private static final int CODE_LENGTH = 8;
+    private static final int CODE_LENGTH = 6;
     private static final SecureRandom RANDOM = new SecureRandom();
     private static final String baseUrl = "http://localhost:8081/";
     private final InvitationRepository invitationRepository;
@@ -45,7 +44,7 @@ public class TeamService {
     @Transactional
     public CreateTeamResponse createTeam(CreateTeamRequest createTeamRequest) {
         // 팀을 만든 user
-        int userId = 2;
+        int userId = 1;
         User user = userRepository.findById(userId).orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_FOUND));
         //1. 팀
         Team savedTeam;
@@ -112,25 +111,20 @@ public class TeamService {
         return new CreateTeamResponse(inviteCode, inviteUrl);
     }
 
-//    public SelectTeamResponse selectTeam(int id) {
-//        SelectTeamResponse selectTeamResponse = teamRepository.findById(id);
-//        return selectTeamResponse;
-//    }
-
     public InsertCodeTeamResponse insertCode(String inviteCode) {
 
         Invitation invitation = invitationRepository.findByInviteCode(inviteCode).orElseThrow(()
                 -> new TeamException(TeamErrorCode.INVITATION_NOT_FOUND));
-        Team team = teamRepository.findById(invitation.getId()).orElseThrow(() -> new TeamException(TeamErrorCode.TEAM_NOT_FOUND));
+        Team team = teamRepository.findById(invitation.getTeam().getId()).orElseThrow(() -> new TeamException(TeamErrorCode.TEAM_NOT_FOUND));
         int invitedMembers = memberRepository.countByTeam(team).orElseThrow(() -> new MemberException(MemberErrorCode.MEMBER_NOT_FOUND));
-        return new InsertCodeTeamResponse(invitation.getId(), invitedMembers);
+        return new InsertCodeTeamResponse(invitation.getTeam().getId(), invitedMembers);
     }
 
     @Transactional
     public DetailPendingTeamResponse selectPendingDetails() {
         //1. 상세 조회 (모임, 규칙, 인원수)
-        int teamId = 2;
-        int userId = 3;
+        int teamId = 1;
+        int userId = 1;
         //2-1. 모임 조회
         Team team = teamRepository.findById(teamId).orElseThrow(() -> new TeamException(TeamErrorCode.TEAM_NOT_FOUND));
         TeamDto teamDto = team.fromEntity(team);
