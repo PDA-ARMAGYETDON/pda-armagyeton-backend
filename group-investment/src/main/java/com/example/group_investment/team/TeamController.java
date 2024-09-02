98a1e1c4e1bd41e8ff765d02bb413f7ffeaf4c12
ymkdev <eveminful@gmail.com>
2024-09-02 09:49:19 +0900

    fix: 초대 코드 참여하기 API, pending 상태 모임 조회 API 엔드포인트 수정
    #41

--- group-investment/src/main/java/com/example/group_investment/team/TeamController.java
+++ group-investment/src/main/java/com/example/group_investment/team/TeamController.java
@@ -1,19 +1,12 @@
 package com.example.group_investment.team;
 
-import com.example.armagyetdon.team.dto.SelectTeamResponse;
-import com.example.armagyetdon.team.dto.TeamDto;
-import com.example.armagyetdon.team.dto.*;
 import com.example.common.dto.ApiResponse;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
-//@RequestMapping("/api/teams")
+@RequestMapping("/api/teams")
 public class TeamController {
 
     private final TeamService teamService;
@@ -30,15 +23,15 @@ public class TeamController {
         return new ApiResponse<>(201, true, "팀을 생성했습니다.", createTeamResponse);
     }
     
-    @GetMapping("/api/groups")
+    @GetMapping()
     public ApiResponse<InsertCodeTeamResponse> insertCode(@RequestParam(value = "inviteCode", required = true) String inviteCode) {
         InsertCodeTeamResponse insertCodeTeamResponse = teamService.insertCode(inviteCode);
         return new ApiResponse<>(200, true, "초대받은 팀으로 입장합니다",insertCodeTeamResponse);
     }
 
-    @GetMapping("/api/groups/{id}/pending/{userId}")
-    public ApiResponse<DetailPendingTeamResponse> selectDetails(@PathVariable int id, @PathVariable int userId) {
-        DetailPendingTeamResponse detailPendingTeamResponse = teamService.selectPendingDetails(id,userId);
+    @GetMapping("/pending")
+    public ApiResponse<DetailPendingTeamResponse> selectDetails() {
+        DetailPendingTeamResponse detailPendingTeamResponse = teamService.selectPendingDetails();
         return new ApiResponse<>(200, true, "초대를 받은 팀 정보를 조회했습니다.", detailPendingTeamResponse);
     }
 }
