69d15202561da62d5a7f5c05d24c76f8dc0bd449
Yoon <kohyc97@gmail.com>
2024-09-02 13:16:21 +0900

    fix: post -> get 매핑으로 변경

--- stock-system/src/main/java/com/example/stock_system/transferHistory/TransferHistoryController.java
+++ stock-system/src/main/java/com/example/stock_system/transferHistory/TransferHistoryController.java
@@ -5,7 +5,6 @@ import com.example.stock_system.transferHistory.dto.AccountTransferDetailDto;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,9 +13,9 @@ public class TransferHistoryController {
     private final TransferHistoryService transferHistoryService;
 
     @Operation(summary = "계좌 이체 내역 조회", description = "모임계좌를 제외한 내 계좌의 이체내역을 조회한다.")
-    @PostMapping("accounts/{id}/transactions")
+    @GetMapping("accounts/{id}/transactions")
     public ApiResponse<AccountTransferDetailDto> getAccountTransferDetail(@PathVariable int id) {
-        
+
         AccountTransferDetailDto accountTransferDetail = transferHistoryService.getAccountTransferDetail(id);
         return new ApiResponse<>(200, true, "계좌 이체 내역 조회에 성공하였습니다.", accountTransferDetail);
 
