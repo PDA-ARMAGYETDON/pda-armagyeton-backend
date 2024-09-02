6fa4ffa5fc6bf469fea6d2c12d83fe29e55e267f
Yoon <kohyc97@gmail.com>
2024-09-02 17:58:54 +0900

    feat: 계좌 정보로 거래내역 조회 후 DTO 로 변환하여 조회 추가

--- stock-system/src/main/java/com/example/stock_system/transferHistory/TransferHistoryRepository.java
+++ stock-system/src/main/java/com/example/stock_system/transferHistory/TransferHistoryRepository.java
@@ -6,16 +6,17 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TransferHistoryRepository extends JpaRepository<TransferHistory, Integer> {
 
-    //TODO: 추후 JWT토큰 도입시 대대적으로 수정해야함(userID로 검색 하게)
     @Query("SELECT new com.example.stock_system.transferHistory.dto.AccountTransferDetailDto(" +
-            "h.account.id, h.transferAt, h.transferAmt, h.receivingAccountId) " +
-            "FROM TransferHistory h LEFT JOIN TeamAccount t ON h.account.id = t.account.id " +
-            "WHERE h.account.id = :accountId AND t.account.id IS NULL")
-    Optional<AccountTransferDetailDto> findByAccountId(@Param("accountId") int userId);
+            "t.account.id, t.transferAt, t.transferAmt, t.receivingAccountId) " +
+            "FROM TransferHistory t " +
+            "WHERE t.account.id = :accountId ")
+    Optional<List<AccountTransferDetailDto>> findByAccountId(@Param("accountId") int accountId);
 
 }
+
