d92059bea91273f95f477f9ead2897b7bdeeeaad
Yoon <kohyc97@gmail.com>
2024-09-02 13:17:24 +0900

    fix: import 최적화

--- stock-system/src/main/java/com/example/stock_system/transferHistory/TransferHistoryController.java
+++ stock-system/src/main/java/com/example/stock_system/transferHistory/TransferHistoryController.java
@@ -4,6 +4,7 @@ import com.example.common.dto.ApiResponse;
 import com.example.stock_system.transferHistory.dto.AccountTransferDetailDto;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
