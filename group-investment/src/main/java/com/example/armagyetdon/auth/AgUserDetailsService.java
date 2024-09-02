bbd25c3cde7c68446a2c612a969d05c99985e794
kyeongseo90 <rudtj9099@gmail.com>
2024-09-02 13:17:22 +0900

    feat: user가 속한 team이 있는지 확인하는 메소드 추가

--- group-investment/src/main/java/com/example/armagyetdon/auth/AgUserDetailsService.java
+++ group-investment/src/main/java/com/example/armagyetdon/auth/AgUserDetailsService.java
@@ -2,19 +2,21 @@ package com.example.armagyetdon.auth;
 
 import com.example.armagyetdon.user.User;
 import com.example.armagyetdon.user.UserRepository;
+import com.example.armagyetdon.user.UserService;
+import com.example.armagyetdon.user.exception.UserErrorCode;
+import com.example.armagyetdon.user.exception.UserException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class AgUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
-
-    public AgUserDetailsService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
 
     @Override
     public UserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
@@ -26,5 +28,11 @@ public class AgUserDetailsService implements UserDetailsService {
         }
 
         return null;
+    }
+
+    @Transactional
+    public boolean isTeamExist(int id) {
+        return userRepository.findById(id).orElseThrow(()->new UserException(UserErrorCode.USER_NOT_FOUND))
+                .getMembers().isEmpty();
     }
 }
