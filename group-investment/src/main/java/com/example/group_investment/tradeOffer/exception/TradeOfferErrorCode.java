c774908ac82545146f0a45f67381d8b3f26b614a
HyeonJu LEE <wkdal3512@dankook.ac.kr>
2024-09-02 16:54:28 +0900

    feat: 증권 시스템 서버와 api 요청 실패 시 사용할 에러코드 추가

--- group-investment/src/main/java/com/example/group_investment/tradeOffer/exception/TradeOfferErrorCode.java
+++ group-investment/src/main/java/com/example/group_investment/tradeOffer/exception/TradeOfferErrorCode.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 @Getter
 public enum TradeOfferErrorCode {
     TRADE_OFFER_NOT_FOUND(404, "AG401", "매매제안이 존재하지 않습니다."),
+    STOCKS_SERVER_BAD_REQUEST(400, "AG001", "증권 시스템 서버 요청에 실패했습니다."),
     ;
 
     private final int status;
