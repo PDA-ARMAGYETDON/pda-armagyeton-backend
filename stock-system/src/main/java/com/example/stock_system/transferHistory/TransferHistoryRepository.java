cde64108b283d4879aca5343e76b099175dbacc5
Yoon <kohyc97@gmail.com>
2024-09-02 09:53:52 +0900

    feat: 이체내역 Repository 팀계좌 제외 후 내계좌 불러오기 쿼리문 작성

--- stock-system/src/main/java/com/example/stock_system/transferHistory/TransferHistoryRepository.java
+++ stock-system/src/main/java/com/example/stock_system/transferHistory/TransferHistoryRepository.java
@@ -1,9 +1,19 @@
 package com.example.stock_system.transferHistory;
 
+import com.example.stock_system.transferHistory.dto.AccountTransferDetailDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface TransferHistoryRepository extends JpaRepository<TransferHistory, Integer> {
+
+    @Query("SELECT new com.example.stock_system.transferHistory.dto.AccountTransferDetailDto(" +
+            "h.account.id, h.transferAt, h.transferAmt, h.receivingAccount.id) " +
+            "FROM TransferHistory h LEFT JOIN TeamAccount t ON h.account.id = t.account.id " +
+            "WHERE h.account.id = :accountId AND t.account.id IS NULL")
+    Optional<AccountTransferDetailDto> findByAccountId(int userId);
 
 }
