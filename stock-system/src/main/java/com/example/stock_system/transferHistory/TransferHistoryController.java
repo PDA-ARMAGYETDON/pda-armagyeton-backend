9fa092bcfb49b472fc4a306b2ecc9389b5707ea6
Yoon <kohyc97@gmail.com>
2024-09-02 10:40:11 +0900

    feat: Swagger설명 추가

--- stock-system/src/main/java/com/example/stock_system/transferHistory/TransferHistoryController.java
+++ stock-system/src/main/java/com/example/stock_system/transferHistory/TransferHistoryController.java
@@ -1,6 +1,7 @@
 package com.example.stock_system.transferHistory;
 
 import com.example.stock_system.transferHistory.dto.AccountTransferDetailDto;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TransferHistoryController {
     private final TransferHistoryService transferHistoryService;
 
+    @Operation(summary = "계좌 이체 내역 조회", description = "모임계좌를 제외한 내 계좌의 이체내역을 조회한다.")
     @PostMapping("accounts/{id}/transactions")
     public AccountTransferDetailDto getAccountTransferDetail(@PathVariable int id) {
 
