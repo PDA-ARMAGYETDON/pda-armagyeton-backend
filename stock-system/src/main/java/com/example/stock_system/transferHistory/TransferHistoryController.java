5eda3110fd5babc9b451caa4538a5c57eee319ee
Yoon <kohyc97@gmail.com>
2024-09-03 13:43:27 +0900

    feat: 모임계좌 이체내역 조회 컨트롤러 추가 및 토큰값 대신 임시값 부여

--- stock-system/src/main/java/com/example/stock_system/transferHistory/TransferHistoryController.java
+++ stock-system/src/main/java/com/example/stock_system/transferHistory/TransferHistoryController.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,13 +14,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class TransferHistoryController {
     private final TransferHistoryService transferHistoryService;
 
-    @Operation(summary = "계좌 이체 내역 조회", description = "모임계좌를 제외한 내 계좌의 이체내역을 조회한다.\n ex)POST ...{유저id}/transactions?page=0&size=10")
-    @GetMapping("/api/accounts/{id}/transactions")
-    public ApiResponse<Page<AccountTransferDetailDto>> getAccountTransferDetail(@PathVariable int id,
-                                                                                @RequestParam int page,
-                                                                                @RequestParam int size) {
+    @Operation(summary = "개인계좌 이체 내역 조회", description = "개인 계좌의 이체내역을 조회.\n ex).../history/private/{id}?page=0&size=10")
+    @GetMapping("/api/accounts/history/private")
+    public ApiResponse<Page<AccountTransferDetailDto>> getPrivateTransferDetail(
+            @RequestParam int page, @RequestParam int size) {
 
-        Page<AccountTransferDetailDto> accountTransferDetailList = transferHistoryService.getPrivateAccountTransferDetail(id, page, size);
+        //FIXME: 토큰 값 아닌 임시로 아이디 부여
+        int id = 1;
+
+        Page<AccountTransferDetailDto> accountTransferDetailList = transferHistoryService
+                .getPrivateAccountTransferDetail(id, page, size);
+        return new ApiResponse<>(200, true, "계좌 이체 내역 조회에 성공하였습니다.", accountTransferDetailList);
+
+    }
+
+    @Operation(summary = "모임계좌 이체 내역 조회", description = "모임계좌의 이체내역을 조회.\n ex).../history/team?page=0&size=10")
+    @GetMapping("/api/accounts/history/team")
+    public ApiResponse<Page<AccountTransferDetailDto>> getTeamTransferDetail(
+            @RequestParam int page, @RequestParam int size) {
+
+        //FIXME: 토큰 값 아닌 임시로 아이디 부여
+        int id = 101;
+
+        Page<AccountTransferDetailDto> accountTransferDetailList = transferHistoryService
+                .getTeamAccountTransferDetail(id, page, size);
         return new ApiResponse<>(200, true, "계좌 이체 내역 조회에 성공하였습니다.", accountTransferDetailList);
 
     }
