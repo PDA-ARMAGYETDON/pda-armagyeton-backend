fb995d097a5f9554759cefb37a7d4c130c3263d4
kyeongseo90 <rudtj9099@gmail.com>
2024-09-03 13:30:06 +0900

    feat: 팀 변경 시 토큰 업데이트 기능

--- group-investment/src/main/java/com/example/group_investment/auth/AuthController.java
+++ group-investment/src/main/java/com/example/group_investment/auth/AuthController.java
@@ -1,11 +1,47 @@
 package com.example.group_investment.auth;
 
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.common.dto.ApiResponse;
+import com.example.group_investment.auth.exception.AuthoErrorCode;
+import com.example.group_investment.auth.exception.AuthoException;
+import com.example.group_investment.auth.utils.JwtUtil;
+import com.example.group_investment.member.MemberRepository;
+import com.example.group_investment.user.User;
+import com.example.group_investment.user.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.AllArgsConstructor;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@AllArgsConstructor
 public class AuthController {
 
+    private final JwtUtil jwtUtil;
+    private final AuthService authService;
 
+    @Operation(summary = "팀 변경 시 => JWT 토큰 갱신")
+    @PutMapping("/api/auth/{teamId}")
+    public ResponseEntity<?> updateToken(@RequestHeader("Authorization") String auth, @PathVariable int teamId){
+
+        String jwtToken = auth.substring(7);
+        if (!jwtUtil.validateToken(jwtToken)){
+            throw new AuthoException(AuthoErrorCode.INVALID_JWT_TOKEN);
+        } else if (jwtUtil.isExpired(jwtToken)) {
+            throw new AuthoException(AuthoErrorCode.EXPIRED_JWT_TOKEN);
+        }
+
+        String newJwtToken = authService.updateToken(auth.substring(7), teamId);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + newJwtToken);
+
+        return new ResponseEntity<>(
+                new ApiResponse<>(200, true, "jwt 토큰이 새롭게 발급되었습니다.", null),
+                headers,
+                HttpStatus.OK);
+    }
 
 }
--- group-investment/src/main/java/com/example/group_investment/auth/AuthService.java
+++ group-investment/src/main/java/com/example/group_investment/auth/AuthService.java
@@ -1,2 +1,41 @@
-package com.example.group_investment.auth;public class AuthService {
+package com.example.group_investment.auth;
+
+import com.example.group_investment.auth.exception.AuthoErrorCode;
+import com.example.group_investment.auth.exception.AuthoException;
+import com.example.group_investment.auth.utils.JwtUtil;
+import com.example.group_investment.member.MemberRepository;
+import com.example.group_investment.member.exception.MemberErrorCode;
+import com.example.group_investment.member.exception.MemberException;
+import com.example.group_investment.user.User;
+import com.example.group_investment.user.UserRepository;
+import com.example.group_investment.user.exception.UserErrorCode;
+import com.example.group_investment.user.exception.UserException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final JwtUtil jwtUtil;
+    private final MemberRepository memberRepository;
+    private final UserRepository userRepository;
+
+    public String updateToken(String jwtToken, int teamId) {
+
+        String currentUserId = jwtUtil.getLoginId(jwtToken);
+        int currentTeamId = jwtUtil.getTeamId(jwtToken);
+
+        User user = userRepository.findByLoginId(currentUserId).orElseThrow(()->new UserException(UserErrorCode.USER_NOT_FOUND));
+        // 팀에 속해있는지 확인
+        if (!memberRepository.existsByUserAndTeamId(user, teamId)) {
+            throw new AuthoException(AuthoErrorCode.NOT_TEAM_MEMBER);
+        }
+
+        String newJwtToken = jwtToken;
+        if (teamId != currentTeamId) {
+            newJwtToken = jwtUtil.createJwt(currentUserId, teamId, jwtUtil.isTeamExist(jwtToken));
+        }
+
+        return newJwtToken;
+    }
 }
