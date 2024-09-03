0720ba86aaa402bab1afbcec7924db8bc9f3d202
kyeongseo90 <rudtj9099@gmail.com>
2024-09-03 13:26:03 +0900

    feat: user 예외 코드 추가

--- group-investment/src/main/java/com/example/group_investment/user/exception/UserErrorCode.java
+++ group-investment/src/main/java/com/example/group_investment/user/exception/UserErrorCode.java
@@ -5,8 +5,7 @@ import lombok.Getter;
 @Getter
 public enum UserErrorCode {
     USER_NOT_FOUND(404, "AG401", "사용자가 존재하지 않습니다."),
-    USER_ALREADY_EXISTS(409, "AG402", "이미 존재하는 아이디입니다."),
-    LOGIN_FAILED(404, "AG403", "로그인에 실패하였습니다.");
+    USER_ALREADY_EXISTS(409, "AG402", "이미 존재하는 아이디입니다.");
 
     private final int status;
     private final String divisionCode;
