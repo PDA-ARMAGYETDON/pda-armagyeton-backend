36c32a0ae559c652292f0b9431d8657ad76ba1ab
ymkdev <eveminful@gmail.com>
2024-09-04 02:02:36 +0900

    feat: 규칙 제안 에러 코드 추가
    #59

--- group-investment/src/main/java/com/example/group_investment/ruleOffer/exception/RuleOfferErrorCode.java
+++ group-investment/src/main/java/com/example/group_investment/ruleOffer/exception/RuleOfferErrorCode.java
@@ -5,7 +5,10 @@ import lombok.Getter;
 @Getter
 public enum RuleOfferErrorCode {
     RULE_OFFER_NOT_FOUND(404, "AG401", "규칙 제안이 존재하지 않습니다."),
+    RULE_OFFER_SAVE_FAILED(400, "AG001", "규칙 제안 저장에 실패했습니다."),
+    RULE_OFFER_UPDATE_FAILED(400, "AG001", "규칙 제안 Update에 실패했습니다.")
     ;
+
 
     private final int status;
     private final String divisionCode;
