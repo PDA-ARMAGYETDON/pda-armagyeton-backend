a4e3802b9862e4399084b9c1e61e9c072eadb087
kyeongseo90 <rudtj9099@gmail.com>
2024-09-03 13:27:59 +0900

    feat: autho 예외 추가

--- group-investment/src/main/java/com/example/group_investment/auth/exception/AuthoException.java
+++ group-investment/src/main/java/com/example/group_investment/auth/exception/AuthoException.java
@@ -3,7 +3,7 @@ package com.example.group_investment.auth.exception;
 import com.example.common.exception.GlobalException;
 
 public class AuthoException extends GlobalException {
-    public AuthoException(AuthErrorCode errorCode) {
+    public AuthoException(AuthoErrorCode errorCode) {
         super(errorCode.getMessage(), null, errorCode.getStatus(), errorCode.getDivisionCode());
     }
 }
