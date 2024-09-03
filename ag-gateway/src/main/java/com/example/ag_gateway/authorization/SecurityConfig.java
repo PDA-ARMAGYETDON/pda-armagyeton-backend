70735339553344a735ee804b37072c1448c95b3c
kyeongseo90 <rudtj9099@gmail.com>
2024-09-03 18:31:14 +0900

    feat: gateway 시큐리티 설정

--- ag-gateway/src/main/java/com/example/ag_gateway/authorization/SecurityConfig.java
+++ ag-gateway/src/main/java/com/example/ag_gateway/authorization/SecurityConfig.java
@@ -1,19 +1,23 @@
-package com.example.ag_gateway;
+package com.example.ag_gateway.authorization;
 
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
-//@EnableWebFluxSecurity
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtUtil jwtUtil;
 
     @Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -26,21 +30,11 @@ public class SecurityConfig {
         http
                 .httpBasic(AbstractHttpConfigurer::disable);
 
-
-
         http
                 .authorizeHttpRequests((requests) -> requests
-                                .requestMatchers("/signin", "/signup").permitAll()
-                                .requestMatchers("/").hasRole("USER")
-                                .anyRequest().authenticated())
-                .logout((logoutConfig)->
-                        logoutConfig
-                                .logoutSuccessUrl("/"))
-//                .exceptionHandling((exceptionHandling) ->
-//                        exceptionHandling
-//                                .authenticationEntryPoint(unauthorizedEntryPoint)
-//                                .accessDeniedPage(accessDeniedHandler))
-//                .userDetailsService(myUserDetailsService());
+//                                .requestMatchers("/api/users/login", "/api/users/signup").permitAll()
+                                .anyRequest().permitAll())
+                .addFilterBefore(new JwtFilter(jwtUtil), BasicAuthenticationFilter.class)
         ;
 
         http
