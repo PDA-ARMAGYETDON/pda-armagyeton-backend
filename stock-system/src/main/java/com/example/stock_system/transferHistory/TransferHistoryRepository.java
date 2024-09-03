416cbbd81217fd109e274c57198d5a90b0cb08ed
Yoon <kohyc97@gmail.com>
2024-09-03 09:31:48 +0900

    feat: findByAccountId JQuery -> 메서드 쿼리 파생으로 변경

--- stock-system/src/main/java/com/example/stock_system/transferHistory/TransferHistoryRepository.java
+++ stock-system/src/main/java/com/example/stock_system/transferHistory/TransferHistoryRepository.java
@@ -2,8 +2,6 @@ package com.example.stock_system.transferHistory;
 
 import com.example.stock_system.transferHistory.dto.AccountTransferDetailDto;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,11 +10,7 @@ import java.util.Optional;
 @Repository
 public interface TransferHistoryRepository extends JpaRepository<TransferHistory, Integer> {
 
-    @Query("SELECT new com.example.stock_system.transferHistory.dto.AccountTransferDetailDto(" +
-            "t.account.id, t.transferAt, t.transferAmt, t.receivingAccountId) " +
-            "FROM TransferHistory t " +
-            "WHERE t.account.id = :accountId ")
-    Optional<List<AccountTransferDetailDto>> findByAccountId(@Param("accountId") int accountId);
+    Optional<List<AccountTransferDetailDto>> findByAccountId(int accountId);
 
 }
 
