63381a5be11ca8fb1fa7067f8cd6755e80ceb1a4
Yoon <kohyc97@gmail.com>
2024-09-04 16:24:09 +0900

    feat: 채팅 모듈 receiver 추가

--- alarm/src/main/java/com/example/alarm/firebase/FcmService.java
+++ alarm/src/main/java/com/example/alarm/firebase/FcmService.java
@@ -2,8 +2,15 @@ package com.example.alarm.firebase;
 
 
 import com.example.alarm.firebase.dto.FcmTokenRequestDto;
+import com.example.alarm.firebase.dto.MqChatDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -15,6 +22,30 @@ public class FcmService {
 
     public void saveFcmToken(FcmTokenRequestDto fcmTokenRequestDto) {
         fcmTokenRepository.save(new FcmToken(fcmTokenRequestDto.getUserId(), fcmTokenRequestDto.getFcmToken()));
+    }
+
+    @RabbitListener(queues = "${spring.rabbitmq.recvChatQueue.name}")
+    public void receiveChatMessage(String message) {
+        try {
+            MqChatDto mqChatDto = new ObjectMapper().readValue(message, MqChatDto.class);
+            System.out.println(" [Chat] 받은 정보: '" + mqChatDto.toString() + "'");
+
+            sendNotificationToTopic(mqChatDto.getTeamId(), mqChatDto.getUserId(), mqChatDto.getChatMessage());
+
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            log.error("Json -> Object convert Error!!");
+        }
+
+    }
+
+    private void sendNotificationToTopic(String topic, String title, String body) {
+        Message fcmMessage = Message.builder()
+                .setTopic(topic)
+                .setNotification(Notification.builder().setTitle(title).setBody(body).build())
+                .build();
+
+        FirebaseMessaging.getInstance().sendAsync(fcmMessage);
     }
 
 //    @Transactional
