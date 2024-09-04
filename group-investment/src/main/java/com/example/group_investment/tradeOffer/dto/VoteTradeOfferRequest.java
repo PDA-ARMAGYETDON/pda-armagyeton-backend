277bbc305e3b7b42d9d135411c87861e324f72ea
HyeonJu LEE <wkdal3512@dankook.ac.kr>
2024-09-04 13:31:39 +0900

    feat: 매매 제안 투표 api의 요청 dto 생성

---
+++ group-investment/src/main/java/com/example/group_investment/tradeOffer/dto/VoteTradeOfferRequest.java
@@ -0,0 +1,9 @@
+package com.example.group_investment.tradeOffer.dto;
+
+import com.example.group_investment.enums.Choice;
+import lombok.Getter;
+
+@Getter
+public class VoteTradeOfferRequest {
+    private Choice choice;
+}
