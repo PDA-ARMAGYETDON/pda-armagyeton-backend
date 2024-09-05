556feae299543a9f7da372e732f52464c50dc4cf
Elandland <bodan99@gmail.com>
2024-09-05 09:50:15 +0900

    feat: TeamId를 통해 teamAccount 조회 생성

--- stock-system/src/main/java/com/example/stock_system/account/TeamAccountRepository.java
+++ stock-system/src/main/java/com/example/stock_system/account/TeamAccountRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TeamAccountRepository extends JpaRepository<TeamAccount, Integer> {
 
+    TeamAccount findByTeamId(int teamId);
+
 }
