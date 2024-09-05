8ccfede50c997a8a313fb87e377b7282e0ac854e
Yoon <kohyc97@gmail.com>
2024-09-05 10:37:33 +0900

    delete: api 제거

--- alarm/src/main/java/com/example/alarm/firebase/FCMController.java
+++ alarm/src/main/java/com/example/alarm/firebase/FCMController.java
@@ -1,11 +1,6 @@
 package com.example.alarm.firebase;
 
-import com.example.common.dto.ApiResponse;
-import com.example.group_investment.user.dto.FcmTokenRequestDto;
-import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,18 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/")
 public class FCMController {
-
-    private final FcmService fcmService;
-
+    
     // UserResponseDto를 만들어서 response해주세요.
     // ApiResponse를 사용해서 response 형식을 통일해주세요.
-
-    @PostMapping("/token")
-    @Operation(summary = "토큰 등록", description = "클라이언트로부터 유저, 팀 정보를 받아와 채팅에 필요한 토큰을 등록합니다.")
-    public ApiResponse getUser(@RequestBody FcmTokenRequestDto fcmTokenRequestDto) {
-        fcmService.saveFcmToken(fcmTokenRequestDto);
-        return new ApiResponse<>(200, true, "토큰을 등록하였습니다.", null);
-    }
 
 
 }
