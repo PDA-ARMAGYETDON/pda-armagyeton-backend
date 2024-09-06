3dc5ca40a54ea46ecd73b767d087f5a24cc040e5
Yoon <kohyc97@gmail.com>
2024-09-06 09:10:55 +0900

    feat: common 에러 처리 기능 추가

--- stock-system/src/main/java/com/example/stock_system/stocks/exception/StocksException.java
+++ stock-system/src/main/java/com/example/stock_system/stocks/exception/StocksException.java
@@ -1,9 +1,15 @@
 package com.example.stock_system.stocks.exception;
 
+import com.example.common.exception.ErrorCode;
 import com.example.common.exception.GlobalException;
 
 public class StocksException extends GlobalException {
+
     public StocksException(StocksErrorCode errorCode) {
+        super(errorCode.getMessage(), null, errorCode.getStatus(), errorCode.getDivisionCode());
+    }
+
+    public StocksException(ErrorCode errorCode) {
         super(errorCode.getMessage(), null, errorCode.getStatus(), errorCode.getDivisionCode());
     }
 }
