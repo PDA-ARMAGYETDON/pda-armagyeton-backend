a8fdcca6c86d9ed48ce890b1636c46fa6d4be2e1
HyeonJu LEE <wkdal3512@dankook.ac.kr>
2024-09-06 17:13:04 +0900

    refactor

--- stock-system/src/main/java/com/example/stock_system/holdings/HoldingsController.java
+++ stock-system/src/main/java/com/example/stock_system/holdings/HoldingsController.java
@@ -22,14 +22,14 @@ public class HoldingsController {
     private final HoldingsService holdingsService;
     private final RealTimeStockService realTimeStockService;
 
-    @Operation(summary = "장 마감 후 계좌 보유 종목 data 조회",description = "DB에서 데이터를 불러오는 API")
+    @Operation(summary = "장 마감 후 계좌 보유 종목 data 조회", description = "DB에서 데이터를 불러오는 API")
     @GetMapping(value = "/{id}")
     public ApiResponse<List<HoldingsDto>> getHoldings(@PathVariable int id) {
         List<HoldingsDto> holdingsList = holdingsService.getHoldings(id);
         return new ApiResponse<>(200, true, "보유종목을 조회했습니다.", holdingsList);
     }
 
-    @Operation(summary = "실시간 계좌 보유 종목 data 조회",description = "실시간 데이터를 불러오는 API")
+    @Operation(summary = "실시간 계좌 보유 종목 data 조회", description = "실시간 데이터를 불러오는 API")
     @GetMapping(value = "/realtime/{id}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<HoldingsDto> getRealTimeHoldings(@PathVariable int id) {
         return holdingsService.getRealTimeHoldings(id, realTimeStockService.getDataOnlyStream());
