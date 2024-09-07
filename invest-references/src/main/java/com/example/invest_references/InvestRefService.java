98345d7983d5353209bcb9cae5f6aa6ce79f8bd8
HyeonJu LEE <wkdal3512@dankook.ac.kr>
2024-09-07 20:41:10 +0900

    feat: 투자 레퍼런스 서비스 클래스 생성

---
+++ invest-references/src/main/java/com/example/invest_references/InvestRefService.java
@@ -0,0 +1,10 @@
+package com.example.invest_references;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class InvestRefService {
+
+}
