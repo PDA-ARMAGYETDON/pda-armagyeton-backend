c5f621a2354ca85e788b1f223daaece568be3af4
HyeonJu LEE <wkdal3512@dankook.ac.kr>
2024-09-07 21:34:19 +0900

    feat: 네이버 증권에서 코스피/코스닥 지수 크롤링하는 로직 추가

--- invest-references/src/main/java/com/example/invest_references/InvestRefService.java
+++ invest-references/src/main/java/com/example/invest_references/InvestRefService.java
@@ -1,5 +1,8 @@
 package com.example.invest_references;
 
+import com.example.invest_references.dto.Kosdaq;
+import com.example.invest_references.dto.Kospi;
+import com.example.invest_references.dto.MarketIndexResponse;
 import com.example.invest_references.dto.News;
 import com.example.invest_references.exception.InvestRefErrorCode;
 import com.example.invest_references.exception.InvestRefException;
@@ -20,18 +23,17 @@ public class InvestRefService {
 
     private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36";
     private static final String NAVER_FINANCE_NEWS_URL = "https://finance.naver.com/news/mainnews.naver";
+    private static final String NAVER_FINANCE_MARKET_INDEX_URL = "https://finance.naver.com/sise/sise_index.naver?code=";
 
     // 네이버 증권 주요 뉴스를 가져오는 메서드
     public List<News> getNews() {
         List<News> newsList = new ArrayList<>();
 
         try {
-            // Jsoup을 사용해 네이버 증권 뉴스 페이지의 HTML 가져오기
             Document doc = Jsoup.connect(NAVER_FINANCE_NEWS_URL)
                     .userAgent(USER_AGENT)
                     .get();
 
-            // 뉴스 리스트를 가져오는 selector (사진에서 제공된 HTML 구조 기반)
             Elements newsElements = doc.select("ul.newsList > li.block1");
 
             // 상위 5개의 뉴스만 파싱
@@ -44,14 +46,53 @@ public class InvestRefService {
                 String url = "https://finance.naver.com" + newsElement.select("a").attr("href");  // 뉴스 URL
                 String date = newsElement.select(".wdate").text().trim();   // 날짜
 
-                // 뉴스 DTO에 저장
-                News newsDTO = new News(press, title, url, date);
-                newsList.add(newsDTO);
+                News news = new News(press, title, url, date);
+                newsList.add(news);
             }
         } catch (IOException e) {
             throw new InvestRefException(InvestRefErrorCode.FAILED_TO_GET_NEWS);
         }
 
         return newsList;
+    }
+
+    public MarketIndexResponse getMarketIndex() {
+        try {
+            Document kospiDoc = Jsoup.connect(NAVER_FINANCE_MARKET_INDEX_URL + "KOSPI")
+                    .userAgent(USER_AGENT)
+                    .get();
+
+            System.out.println("kospiDoc = " + kospiDoc.title());
+
+            String kospiNowValue = kospiDoc.select("em#now_value").text().trim();
+            String kospiChangeValueAndRate = kospiDoc.select("span#change_value_and_rate").text().trim();
+            String kospiChangeValue = kospiChangeValueAndRate.split(" ")[0];
+            String kospiChangeRate = kospiChangeValueAndRate.split(" ")[1].replaceAll("[^0-9-.%]", "");
+
+            Document kosdaqDoc = Jsoup.connect(NAVER_FINANCE_MARKET_INDEX_URL + "KOSDAQ")
+                    .userAgent(USER_AGENT)
+                    .get();
+
+            String kosdaqNowValue = kosdaqDoc.select("em#now_value").text().trim();
+            String kosdaqChangeValueAndRate = kosdaqDoc.select("span#change_value_and_rate").text().trim();
+            String kosdaqChangeValue = kosdaqChangeValueAndRate.split(" ")[0];
+            String kosdaqChangeRate = kosdaqChangeValueAndRate.split(" ")[1].replaceAll("[^0-9-.%]", "");
+
+            return MarketIndexResponse.builder()
+                    .kospi(Kospi.builder()
+                            .currentValue(kospiNowValue)
+                            .changeValue(kospiChangeValue)
+                            .changeRate(kospiChangeRate)
+                            .build())
+                    .kosdaq(Kosdaq.builder()
+                            .currentValue(kosdaqNowValue)
+                            .changeValue(kosdaqChangeValue)
+                            .changeRate(kosdaqChangeRate)
+                            .build())
+                    .build();
+
+        } catch (IOException e) {
+            throw new InvestRefException(InvestRefErrorCode.FAILED_TO_GET_MARKET_INDEX);
+        }
     }
 }
