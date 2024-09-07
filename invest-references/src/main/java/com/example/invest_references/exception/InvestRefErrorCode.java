c44cd683a931179d82f382c3c6d9d9037a8b569c
HyeonJu LEE <wkdal3512@dankook.ac.kr>
2024-09-07 21:34:39 +0900

    feat: 코스피/코스닥 지수 조회 관련 에러코드 추가

--- invest-references/src/main/java/com/example/invest_references/exception/InvestRefErrorCode.java
+++ invest-references/src/main/java/com/example/invest_references/exception/InvestRefErrorCode.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public enum InvestRefErrorCode {
     FAILED_TO_GET_NEWS(400, "AG001", "네이버 증권 뉴스를 가져오는 데 실패했습니다."),
-    ;
+    FAILED_TO_GET_MARKET_INDEX(400, "AG002", "코스피/코스닥 지수를 가져오는 데 실패했습니다.");
 
     private final int status;
     private final String divisionCode;
