478f7feeeb893759edee591efa943f6963170997
kyeongseo90 <rudtj9099@gmail.com>
2024-09-07 16:50:19 +0900

    feat: security로 Cors 에러 처리

--- ag-gateway/src/main/java/com/example/ag_gateway/config/SecurityConfig.java
+++ ag-gateway/src/main/java/com/example/ag_gateway/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.example.ag_gateway.authorization.JwtFilter;
 import com.example.ag_gateway.authorization.JwtUtil;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,6 +14,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -21,10 +27,29 @@ public class SecurityConfig {
 
     private final JwtUtil jwtUtil;
 
+    @Value("${url.client}")
+    private String clientUrl;
+
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedOriginPatterns(Collections.singletonList(clientUrl)); // ⭐️ Client
+        config.setAllowCredentials(true);
+        config.setExposedHeaders(Collections.singletonList("Authorization"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
     @Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable);
+
+        http
+                .cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()));
 
         http
                 .formLogin(AbstractHttpConfigurer::disable);
