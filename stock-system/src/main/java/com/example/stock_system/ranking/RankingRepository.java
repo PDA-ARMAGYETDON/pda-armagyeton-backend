016e7cb98b60331bf07353744b10c1cf8be25d1f
ymkdev <eveminful@gmail.com>
2024-09-08 01:20:54 +0900

    feat: 랭킹 repository 생성

---
+++ stock-system/src/main/java/com/example/stock_system/ranking/RankingRepository.java
@@ -0,0 +1,16 @@
+package com.example.stock_system.ranking;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface RankingRepository extends JpaRepository<Ranking, Integer> {
+    @Query("SELECT r FROM Ranking r WHERE r.seedMoney >= :minSeedMoney AND r.seedMoney <= :maxSeedMoney ORDER BY r.evluPflsRt DESC")
+    Optional<List<Ranking>> findBySeedMoneyAndOrderByEvluPflsRt(@Param("minSeedMoney")int minSeedMoney,@Param("maxSeedMoney") int maxSeedMoney);
+
+}
