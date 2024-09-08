2028376f1ed11ff5a417fc42dabad042cb8e0364
Yoon <kohyc97@gmail.com>
2024-09-08 21:02:35 +0900

    feat: 규칙 투표 알림 DTO 생성

---
+++ group-investment/src/main/java/com/example/group_investment/ruleOffer/dto/VoteRuleToAlarmDTO.java
@@ -0,0 +1,11 @@
+package com.example.group_investment.ruleOffer.dto;
+
+public class VoteRuleToAlarmDTO {
+    private int teamId;
+    private String teamName;
+
+    public VoteRuleToAlarmDTO(int teamId, String teamName) {
+        this.teamId = teamId;
+        this.teamName = teamName;
+    }
+}
