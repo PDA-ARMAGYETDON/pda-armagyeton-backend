ab625accd4f004a3a1939ad70c6fa9a42077b9a5
Yoon <kohyc97@gmail.com>
2024-09-10 08:56:48 +0900

    fix: DTO 명 변경

--- group-investment/src/main/java/com/example/group_investment/ruleOffer/dto/VoteRuleToAlarmDto.java
+++ group-investment/src/main/java/com/example/group_investment/ruleOffer/dto/VoteRuleToAlarmDto.java
@@ -1,15 +1,11 @@
 package com.example.group_investment.ruleOffer.dto;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Getter
-@NoArgsConstructor
-public class VoteRuleToAlarmDto {
+public class VoteRuleToAlarmDTO {
     private int teamId;
     private String teamName;
 
-    public VoteRuleToAlarmDto(int teamId, String teamName) {
+    public VoteRuleToAlarmDTO(int teamId, String teamName) {
         this.teamId = teamId;
         this.teamName = teamName;
     }
