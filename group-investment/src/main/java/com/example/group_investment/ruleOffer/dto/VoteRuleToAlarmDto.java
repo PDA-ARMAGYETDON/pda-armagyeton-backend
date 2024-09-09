dc500f6443d76f6ec36f351d3f287ee95e9d5bb3
Yoon <kohyc97@gmail.com>
2024-09-10 08:24:03 +0900

    feat: DTO 기본 생성자 추가 및 getter 추가

--- group-investment/src/main/java/com/example/group_investment/ruleOffer/dto/VoteRuleToAlarmDto.java
+++ group-investment/src/main/java/com/example/group_investment/ruleOffer/dto/VoteRuleToAlarmDto.java
@@ -1,5 +1,10 @@
 package com.example.group_investment.ruleOffer.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class VoteRuleToAlarmDto {
     private int teamId;
     private String teamName;
