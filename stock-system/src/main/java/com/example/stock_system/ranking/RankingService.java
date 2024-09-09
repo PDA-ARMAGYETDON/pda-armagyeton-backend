83c0a535e4daf1e74a7b32e414e1206204d335bd
Elandland <bodan99@gmail.com>
2024-09-09 11:32:57 +0900

    feat. 팀이 생성 될때 ranking table에 data넣는 로직 추가

--- stock-system/src/main/java/com/example/stock_system/ranking/RankingService.java
+++ stock-system/src/main/java/com/example/stock_system/ranking/RankingService.java
@@ -1,13 +1,24 @@
 package com.example.stock_system.ranking;
 
+import com.example.common.dto.ApiResponse;
+import com.example.stock_system.account.Account;
+import com.example.stock_system.account.TeamAccount;
+import com.example.stock_system.account.TeamAccountRepository;
+import com.example.stock_system.account.exception.AccountErrorCode;
+import com.example.stock_system.account.exception.AccountException;
+import com.example.stock_system.enums.Category;
 import com.example.stock_system.ranking.dto.SelectRankingResponse;
 import com.example.stock_system.ranking.dto.SelectRankingWithTeamResponse;
 import com.example.stock_system.ranking.exception.RankingErrorCode;
 import com.example.stock_system.ranking.exception.RankingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,7 +26,12 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class RankingService {
 
+    @Value("${ag.url}")
+    private String AG_URL;
+
     private final RankingRepository rankingRepository;
+    private final TeamAccountRepository teamAccountRepository;
+    private final RestTemplate restTemplate;
 
     public SelectRankingWithTeamResponse selectRanking(int teamId, int maxSeedMoney) {
         int minSeedMoney;
@@ -49,5 +65,19 @@ public class RankingService {
                 .teamRanking(teamRanking)
                 .rankings(rankingResponses)
                 .build();
+    }
+
+    public void registRanking(Account savedAccount){
+        TeamAccount teamAccount = teamAccountRepository.findByAccount(savedAccount).orElseThrow(()-> new AccountException(AccountErrorCode.ACCOUNT_NOT_FOUND));
+        int teamId = teamAccount.getTeamId();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        String url = AG_URL+"/api/group/backend/team-category?teamId="+teamId;
+
+        ResponseEntity<ApiResponse> response = restTemplate.getForEntity(url, ApiResponse.class);
+        Category teamCategory = objectMapper.convertValue(response.getBody().getData(), new TypeReference<Category>() {});
+
+        rankingRepository.save(new Ranking(teamCategory,savedAccount,teamId,savedAccount.getDeposit(),savedAccount.getTotalEvluPflsRt()));
     }
 }
