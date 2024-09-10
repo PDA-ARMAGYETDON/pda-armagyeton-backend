d46c6be5f51e7c06032a365f06872ef952325574
Elandland <bodan99@gmail.com>
2024-09-10 16:32:11 +0900

    feat: 배치 처리한 test컨트롤러 삭제

--- stock-system/src/main/java/com/example/stock_system/account/AccountController.java
+++ stock-system/src/main/java/com/example/stock_system/account/AccountController.java
@@ -5,7 +5,6 @@ import com.example.stock_system.account.dto.*;
 import com.example.stock_system.holdings.HoldingsService;
 import com.example.stock_system.holdings.dto.HoldingsDto;
 import com.example.stock_system.ranking.RankingService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
@@ -40,13 +39,6 @@ public class AccountController {
         return new ApiResponse<>(201, true, "모임 계좌가 생성되었습니다.", null);
     }
 
-    @Operation(summary = "자동 이체 서비스", description = "test를 위해 호출식으로 작성, 추후에 배치로 처리")
-    @PostMapping("/auto")
-    public void autoPaymentAndExpel() {
-        List<AccountPayment> accountPayments = accountService.convertPaymentData();
-        List<PayFail> payFails = accountService.autoPaymentService(accountPayments);
-        accountService.expelMember(payFails);
-    }
 
     @Operation(summary = "실시간 계좌 보유 종목들의 각 data 조회", description = "실시간 데이터를 불러오는 API")
     @GetMapping(value = "/realtime/{teamId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
@@ -58,27 +50,6 @@ public class AccountController {
     @GetMapping(value = "/sum-realtime/{teamId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<GetTeamAccountResponse> streamRealTimeSumByTeamId(@PathVariable int teamId) {
         return accountService.getRealTimeSumByTeamId(teamId);
-    }
-
-    @Operation(summary = "자동 전량 매도 서비스", description = "test를 위해 호출식으로 작성, 추후에 배치로 처리")
-    @GetMapping("/all-stock-sell/{teamId}")
-    public ApiResponse<Integer> allStockSell(@PathVariable int teamId) {
-        int finishedTeam = accountService.allStockSell(teamId);
-        return new ApiResponse<>(200, true, "해당 계좌의 주식이 전량 매도 되었습니다.", finishedTeam);
-    }
-
-    @Operation(summary = "랭킹 업데이트", description = "test 위해 작성, 추후 배치 처리")
-    @GetMapping("/update/ranking")
-    public ApiResponse updateRanking() {
-        accountService.updateRanking();
-        return new ApiResponse<>(201, true, "랭킹 업데이트합니다.", null);
-    }
-
-    @Operation(summary = "장 종료후 수익률체크", description = "최대 수익률 넘거나, 최소 수익률보다 낮으면 전량매도, 추후 배치로 바꿔야됨 test위해 컨트롤러로 처리")
-    @GetMapping("/test")
-    public ApiResponse test() throws JsonProcessingException {
-        accountService.checkDisband();
-        return new ApiResponse<>(200, true, "해당 계좌의 주식이 전량 매도 되었습니다.", null);
     }
 
     @Operation(summary = "개인계좌 조회", description = "개인 계좌 조회")
