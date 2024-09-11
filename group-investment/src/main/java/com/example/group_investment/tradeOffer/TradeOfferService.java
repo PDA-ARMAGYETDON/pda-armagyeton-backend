bdaa159c90bd0f9a1d3bf29594943583b3b2f097
Yoon <kohyc97@gmail.com>
2024-09-11 11:29:19 +0900

    fix: 큐 이름 변경

--- group-investment/src/main/java/com/example/group_investment/tradeOffer/TradeOfferService.java
+++ group-investment/src/main/java/com/example/group_investment/tradeOffer/TradeOfferService.java
@@ -24,6 +24,8 @@ import com.example.group_investment.tradeOffer.utils.TradeOfferConverter;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
@@ -39,6 +41,7 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class TradeOfferService {
+    private static final Logger log = LoggerFactory.getLogger(TradeOfferService.class);
     private final TradeOfferRepository tradeOfferRepository;
     private final MemberRepository memberRepository;
     private final TeamRepository teamRepository;
@@ -49,9 +52,8 @@ public class TradeOfferService {
     private final TradeOfferCommunicator tradeOfferCommunicator;
     private final RabbitTemplate rabbitTemplate;
 
-    @Value("${spring.rabbitmq.vote-alarm-queue.name}")
-    private String voteToAlarmQueueName;
-
+//    @Value("${spring.rabbitmq.vote-alarm-queue.name}")
+//    private String voteToAlarmQueueName;
 
     @Value("${spring.rabbitmq.main-stock-queue.name}")
     private String mainStockQueue;
@@ -90,8 +92,8 @@ public class TradeOfferService {
             //json 으로 직렬화 하여 전송
             ObjectMapper objectMapper = new ObjectMapper();
             String objToJson = objectMapper.writeValueAsString(message);
-
-            rabbitTemplate.convertAndSend(voteToAlarmQueueName, objToJson);
+            log.info("주식 매매 제안 알림 전송 from TradeOfferService");
+            rabbitTemplate.convertAndSend("vote_to_alarm", objToJson);
 
 
         } catch (JsonProcessingException e) {
